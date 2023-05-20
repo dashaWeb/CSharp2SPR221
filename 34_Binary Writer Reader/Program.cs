de8ed40b38111ff216bd21ee6de9a3201b15ef94
dashaWeb <57445558+dashaWeb@users.noreply.github.com>
2023-05-20 11:38:40 +0300

    Stream Read and Write

--- 34_Binary Writer Reader/Program.cs
+++ 34_Binary Writer Reader/Program.cs
@@ -7,7 +7,7 @@ namespace _34_Binary_Writer_Reader
     {
         static void Main(string[] args)
         {
-            string fname = "my.dat";
+            /*string fname = "my.dat";
             using (BinaryWriter bw = new BinaryWriter(new FileStream(fname, FileMode.Create)))
             {
                 string test = "Test-line Тестовий рядок";
@@ -27,7 +27,7 @@ namespace _34_Binary_Writer_Reader
 
             }
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            using (BinaryReader br = new BinaryReader(new FileStream(fname,FileMode.Open)))
+            using (BinaryReader br = new BinaryReader(new FileStream(fname, FileMode.Open)))
             {
                 string readStr = br.ReadString();
                 Console.WriteLine($"Read str :: {readStr}");
@@ -35,6 +35,37 @@ namespace _34_Binary_Writer_Reader
                 Console.WriteLine($"int      :: {br.ReadInt32()}");
 
                 Console.WriteLine("Recovered Array :: ");
+                int[] readArray = new int[br.ReadInt32()];
+                for (int i = 0; i < readArray.Length; i++)
+                {
+                    readArray[i] = br.ReadInt32();
+                    Console.WriteLine($"{readArray[i],7}");
+                }
+
+            }*/
+            int[] arr = new int[] { };
+
+            Console.WriteLine($"Enter number(int(0-255)) :");
+            int j = 0;
+            do
+            {
+                Array.Resize<int>(ref arr, arr.Length + 1);
+                arr[j] = Convert.ToInt32(Console.ReadLine());
+                j++;
+            } while (j <=10);
+            //Console.WriteLine($"{arr.Length}");
+            string filename = "myintarr.dat";
+            using (BinaryWriter bw = new BinaryWriter(new FileStream(filename, FileMode.Create)))
+            {
+                bw.Write(arr.Length);
+                foreach (var item in arr)
+                {
+                    bw.Write(item);
+                }
+
+            }
+            using (BinaryReader br = new BinaryReader(new FileStream(filename, FileMode.Open)))
+            {
                 int[] readArray = new int[br.ReadInt32()];
                 for (int i = 0; i < readArray.Length; i++)
                 {
---
+++ 35_Stream write text/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Text;
+namespace _35_Stream_write_text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fname = "info.txt";
+            using (StreamWriter sw = new StreamWriter(fname))
+            {
+                string line = "Hello from C# Streams";
+                int value = 45646;
+                DateTime today = DateTime.Now;
+                int[] array = { 10, 20, 30, 40, 50 };
+
+                sw.WriteLine(line);
+                sw.WriteLine($"Value :: {value}");
+                sw.WriteLine(today);
+                sw.WriteLine(array.Length);
+                foreach (var item in array)
+                {
+                    sw.WriteLine(item);
+                }
+            }
+            //way 1
+            Console.WriteLine($"Content (File.ReadAllText):: \n\t {File.ReadAllText(fname)}");
+            Console.WriteLine();
+
+            //way 2
+            Console.WriteLine("Content (File.ReadAllLines)\n");
+            string[] lines = File.ReadAllLines(fname);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                Console.WriteLine($"{i + 1} ::::: {lines[i]}");
+            }
+
+            //way 3
+            using (StreamReader sr = new StreamReader(fname))
+            {
+                string line;
+                Console.WriteLine("\n\n Content (line by line)");
+                /*while (!sr.EndOfStream)
+                {
+                    line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }*/
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+
+            // way 4
+            using (StreamReader sr = new StreamReader(fname))
+            {
+                Console.WriteLine($"\n\n Content (ReadEnd) :: {sr.ReadToEnd()}");
+            }
+
+            //way 5 
+            using (StreamReader sr = new StreamReader(fname))
+            {
+                Console.WriteLine($"\n\n Content (char by char) :: ");
+                int symbol;
+                while((symbol = sr.Read())!= -1)
+                {
+                    Console.WriteLine((char)symbol);
+                }
+            }
+        }
+    }
+}
+// Дано текстовий документ (створити у Блокноті чи студії). Знайти число рядків, кількість символів, число слів, 
+// * кількість цифр
+// * кількість голосних букв
+// * кількість приголосних букв
